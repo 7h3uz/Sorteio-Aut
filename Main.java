07d8080e7100e31f815ae3e8b8e3c051019cb00e
7h3uz <82480792+7h3uz@users.noreply.github.com>
2022-02-22 19:13:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+	
+	public static void main(String args[]) {
+		Scanner in = new Scanner (System.in);
+		int sort=0;
+		sort=1+(int) (Math.random()*170);
+		System.out.println(sort+" É o ganhador ou ganhadora do sorteio!");
+	}
+}
